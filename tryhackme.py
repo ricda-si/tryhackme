ca34a600ce42d6f3ceb40d7d18c4475630daca1a
ricda-si <ric.ssr@proton.me>
2025-06-12 15:55:12 +0100

    .

--- tryhackme.py
+++ tryhackme.py
@@ -1,8 +1,15 @@
 import os
 import sys
+import const
 
 class TryHackMe:
     def __init__(self):
-        print("Iniciating. . .")
+        os.system("clear")
+        self.get_ovpn_file()
+
+    def get_ovpn_file(self):
+        for file in os.listdir(const.PATH):
+            print(file)
+
 
 tryhackme = TryHackMe()
