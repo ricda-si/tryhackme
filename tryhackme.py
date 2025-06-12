55be9bcebf7c4a52a3eab49634411da84f6ff431
ricda-si <ric.ssr@proton.me>
2025-06-12 17:30:45 +0100

    .

--- tryhackme.py
+++ tryhackme.py
@@ -1,13 +1,13 @@
 import os
+from sys import exit
 import subprocess
 
 class TryHackMe:
     def __init__(self):
         os.system("clear")
         self.check_sudo()
-        self.connected = False
         self.vpn_conn()
-
+        self.run_scan()
 
     def check_sudo(self):
         while True:
@@ -22,5 +22,17 @@ class TryHackMe:
         os.chdir(path)
         ovpn_file = "0xPsyBxxst.ovpn"
         subprocess.run(["sudo", "openvpn", "--config", ovpn_file, "--daemon"], check=True)
+
+    def stop_conn(self):
+        os.system("sudo pkill openvpn")
+        os.system("ps aux | grep openvpn")
+        print("Connection Stopped!")
+
+    def run_scan(self):
+        print("Scanning. . .")
+        sair = input("Exit? y/n> ").lower()
+        if sair == 'y':
+            self.stop_conn()
+            exit()
 
 tryhackme = TryHackMe()
